ef2ca39e6982c5919e14761c8d30d5ec9c14f2b8
nathsevage <nathsevage@gmail.com>
2024-02-05 20:44:33 -0300

    adicionado api

--- asserts/js/main.js
+++ asserts/js/main.js
@@ -1,9 +1,18 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+function convertPokemonToLi(pokemon) {
+  return `<li class= 'pokemon'>
+                <span class="number">001</span>
+                <><span class="name">${pokemon.name}</span><div class="detail">
+                        <ol class="types">
+                            <li class="type">grass</li>
+                            <li class="type">poison</li>
+                        </ol>
+                        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="${pokemon.name}">
+                        </></div></>
+            </li>`;
+}
 
-fetch(url)
-    .then( (response) => response.json())
-    .then ((jsonBody) => console.log(responseBody))
-    .catch ((error) => console.log(error))
+const pokemonList = document.getElementById("pokemonList");
 
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML == pokemons.map(convertPokemonToLi).join("");
+});
